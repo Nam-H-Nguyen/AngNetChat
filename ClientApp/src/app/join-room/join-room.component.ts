07e7e4cc31f7b94682530385b07965ff26b5b167
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2024-08-26 08:54:24 -0700

    Add html, css, validation to join room component

--- ClientApp/src/app/join-room/join-room.component.ts
+++ ClientApp/src/app/join-room/join-room.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join-room',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './join-room.component.html',
   styleUrl: './join-room.component.css'
 })
-export class JoinRoomComponent {
+export class JoinRoomComponent implements OnInit {
+  joinRoomForm!: FormGroup;
+  formBuilder = inject(FormBuilder);
+  router = inject(Router);
 
+  ngOnInit(): void {
+    this.joinRoomForm = this.formBuilder.group({
+      user: ['', Validators.required],
+      room: ['', Validators.required]
+    });
+  }
+
+  joinRoom(){
+    this.router.navigate(['chat']);
+  }
 }
