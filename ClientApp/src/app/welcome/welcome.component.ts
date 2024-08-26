335e4e7955f9c658c08344f49fe9e649d9b53155
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2024-08-26 09:18:52 -0700

    Add template for welcome and chat pages

--- ClientApp/src/app/welcome/welcome.component.ts
+++ ClientApp/src/app/welcome/welcome.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './welcome.component.html',
+  template: `
+    <button [routerLink]="['/join-room']">Join Room Now</button>
+  `,
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
